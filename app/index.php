6e0761ac61ecc0aa4d846b3ff454ddb5fb6f4c15
GabrielMur547 <gabrielmurillo547@gmail.com>
2019-11-16 16:08:51 -0500

    lol

--- app/index.php
+++ app/index.php
@@ -3,15 +3,15 @@ $conn = new mysqli("mysql", "root", "clave123", "Secretaria_Administrativa");
 // Verifica conexión
 if ($conn->connect_error) {
 	die("La conexión falló: " . $conn->connect_error);
-}else echo "Conexión exitosa ";
+}else echo "Conexión exitosa "."<br>";
 
-$sql = "SELECT nombre FROM nombre";
+$sql = "SELECT nombre, apellido FROM usuarios";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 	// Datos de salida de cada registro
 	while($row = $result->fetch_assoc()) {
-		echo $row['nombre']."<br>";
+		echo $row["nombre"].$row["apellido"]."<br>";
 	}
 } else {
 	echo "Ningún resultado para la tabla usuarios";
