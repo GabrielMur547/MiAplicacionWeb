df11f2535930ef3ac860845a54a0f47acccff9c5
Anthony Santana <santhony0398@gmail.com>
2019-12-02 13:04:27 -0500

    Cambios del menu principal

--- app/Views/Layouts/menu.php
+++ app/Views/Layouts/menu.php
@@ -1,31 +1,44 @@
-<div class="top">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <img src="http://www.utp.ac.pa/sites/default/files/SISTEMAS-LOGO.png" style="width:4%; padding-left:10px; position:center" />
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Secretaría Administrativa<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?controller=Docente&action=index">Docentes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?controller=Administrativo&action=index">Administrativo</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Reparaciones</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Usuario
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Perfil</a>
-                            <a class="dropdown-item" href="#">Trabajo</a>
-                            <a class="dropdown-item" href="#">Cerrar Sesión</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+<div style="background-color: green;">
+<nav class="navbar navbar-expand-lg navbar-light" >
+    <button style="border-color:white;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01" style="background-color: green;">
+        <img src="http://www.utp.ac.pa/sites/default/files/SISTEMAS-LOGO.png" style="width:4%; position:center" />
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a style="color:white;font-size:16px;" class="nav-link" href="#">Secretaría Académica<span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a style="color:white; font-size:16px;" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Gestión Docente
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="?controller=Administrativo&action=index">Listar Docentes</a>
+                    <a class="dropdown-item" href="#">Agregar Docente</a>
+                </div>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a style="color:white; font-size:16px;" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Gestión Administrativa
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="?controller=Administrativo&action=index">Listar Administrativo</a>
+                    <a class="dropdown-item" href="#">Agregar Administrativo</a>
+                </div>
+            </li>
+        </ul>
+        <div class="dropdown">
+  <button style="background-color: green; border-color:white;" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Nombre Usuario
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+    <button class="dropdown-item" type="button">Perfil</button>
+    <button class="dropdown-item" type="button">Cerrar Sesión</button>
+  </div>
+</div>
     </div>
+</nav>
+</div>
