6b0dfa8f34b20514d2eab81838eaea8c182c5ee2
GabrielMur547 <gabrielmurillo547@gmail.com>
2019-12-01 21:09:25 -0500

    Update db.php

--- app/Db/db.php
+++ app/Db/db.php
@@ -5,7 +5,7 @@ class Conexion
 
     public static function conectar()
     {
-        $conexion = new mysqli("mysql", "root", "contra123", "Secretaria_Administrativa");
+        $conexion = new mysqli("mysql", "root", "clave123", "Secretaria_Administrativa");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
