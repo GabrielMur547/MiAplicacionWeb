56a5c4f752a00aa1bc3fb359499df4c949825470
GabrielMur547 <gabrielmurillo547@gmail.com>
2019-12-04 20:04:19 -0500

    todo bien

--- Administracion SQL/trigger.sql
+++ Administracion SQL/trigger.sql
@@ -1,8 +1,8 @@
 use Secretaria_Administrativa
 
 DELIMITER |
-Create trigger registrar 
-after insert on registro for each row 
+Create trigger registrar1 
+after insert on registrar for each row 
 begin 
 insert into docentes(primer_nombre, primer_apellido, cedula, correo)
 values(new.primer_nombre, new.primer_apellido, new.cedula, new.correo); 
