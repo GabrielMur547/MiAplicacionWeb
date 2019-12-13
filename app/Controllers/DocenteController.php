1cb38d195877199f5f812c5b31d77d503b1b905e
Anthony Santana <santhony0398@gmail.com>
2019-12-13 18:17:51 -0500

    Vista de edicion de perfil

--- app/Controllers/DocenteController.php
+++ app/Controllers/DocenteController.php
@@ -28,6 +28,29 @@ class DocenteController
 		require_once('Views/Docente/add.php');
 	}
 
+	function update(){
+		require_once('Views/Perfil/actualizar.php');
+	}
+
+	function save_update(){
+		$docente = new DocenteModel();
+		if(isset($_POST['correo']) && isset($_POST['contrasenia']) && isset($_POST['nombre']) && isset($_POST['apellido'])&& isset($_POST['cedula'])){
+		$correo = $_POST['correo'];
+		$password = $_POST['contrasenia'];
+		$nombre = $_POST['nombre'];
+		$apellido = $_POST['apellido'];
+		$cedula = $_POST['cedula'];
+
+		if($docente->insert_user($correo, $password, $nombre, $apellido, $cedula)){
+		require_once('Views/Docente/index.php');
+		}
+		else{
+			$errorRegister = "El usuario ya existe";
+			require_once('Views/Docente/add.php');
+		}
+	}
+	}
+
 	function save(){ 
 		
 		$docente = new DocenteModel();
