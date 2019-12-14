7d57fe62035665e2da0a0e333c0e6354848adaff
Anthony Santana <santhony0398@gmail.com>
2019-12-14 01:28:24 -0500

    Sintaxis BD

--- app/Models/PerfilModel.php
+++ app/Models/PerfilModel.php
@@ -10,55 +10,57 @@ class PerfilModel{
         $user = new UserSessionController();
         $id = $this->db->query("select id_docente from docentes where correo = '$user';");
     }
-    public function update_perfil($primer_nombre, 
-    $primer_apellido,
-    $segundo_nombre, 
-    $segundo_apellido, 
-    $cedula, 
-    $fecha_nac, 
-    $estado_civil, 
-    $tipo_sangre,
-    $telefono_casa,
-    $celular,
-    $provincia_res,
-    $distrito_res,
-    $corregimiento_res,
-    $direccion_especif, 
-    $categoria_doc, 
-    $ubicacion_cede, 
-    $depto, 
-    $correo, 
-    $genero){ 
+    public function update_perfil(  $primer_nombre, 
+                                    $primer_apellido,
+                                    $segundo_nombre, 
+                                    $segundo_apellido, 
+                                    $cedula, 
+                                    $fecha_nac, 
+                                    $estado_civil, 
+                                    $tipo_sangre,
+                                    $telefono_casa,
+                                    $celular,
+                                    $provincia_res,
+                                    $distrito_res,
+                                    $corregimiento_res,
+                                    $direccion_especif, 
+                                    $categoria_doc, 
+                                    $ubicacion_cede, 
+                                    $depto, 
+                                    $correo, 
+                                    $genero){ 
 
-    $consultar = "SELECT * FROM docentes WHERE cedula = '$cedula';";
-        $result = $this->db->query($consultar);
+        $consultar = "SELECT * FROM docentes WHERE cedula = '$cedula';";
+            $result = $this->db->query($consultar);
 
             if(mysqli_num_rows($result)>0){
-            $insert_user = $this->db->query("UPDATE docentes SET (primer_nombre='$primer_nombre', 
-                segundo_nombre='$segundo_nombre', 
-                primer_apellido='$primer_apellido', 
-                segundo_apellido='$segundo_apellido', 
-                cedula='$cedula', 
-                fecha_nac='$fecha_nac', 
-                estado_civil='$estado_civil', 
-                tipo_sangre='$tipo_sangre',
-                telefono_csa='$telefono_casa',
-                celular='$celular',
-                provincia_res='$provincia_res',
-                distrito_res='$distrito_res',
-                corregimiento_res='$corregimiento_res',
-                direcion_especif'$direccion_especif', 
-                categoria_doc='$categoria_doc', 
-                ubicacion_cede='$ubicacion_cede', 
-                depto='$depto', 
-                correo='$correo', 
-                genero='$genero') WHERE cedula=$cedula;"); //Colocar usuario actual    
-    return true;
+                $insert_user = $this->db->query("UPDATE docentes SET (primer_nombre='$primer_nombre', 
+                                                                    segundo_nombre='$segundo_nombre', 
+                                                                    primer_apellido='$primer_apellido', 
+                                                                    segundo_apellido='$segundo_apellido', 
+                                                                    cedula='$cedula', 
+                                                                    fecha_nac='$fecha_nac', 
+                                                                    estado_civil='$estado_civil', 
+                                                                    tipo_sangre='$tipo_sangre',
+                                                                    telefono_csa='$telefono_casa',
+                                                                    celular='$celular',
+                                                                    provincia_res='$provincia_res',
+                                                                    distrito_res='$distrito_res',
+                                                                    corregimiento_res='$corregimiento_res',
+                                                                    direcion_especif'$direccion_especif', 
+                                                                    categoria_doc='$categoria_doc', 
+                                                                    ubicacion_cede='$ubicacion_cede', 
+                                                                    depto='$depto', 
+                                                                    correo='$correo', 
+                                                                    genero='$genero') WHERE cedula=$cedula;"); //Colocar usuario actual    
+                return true;
             }
             else{
                 return false;
             }
-}
+    }
+
+    
     public function insert_familiar($parentezco,
     $nombre_fam,
     $apellido_fam,
