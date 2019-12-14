55c088f778cd7cc1638a6e92a74eafef4e7e8124
Anthony Santana <santhony0398@gmail.com>
2019-12-14 17:46:57 -0500

    Autollenado de campos del form

--- app/Views/Perfil/Actualizar.php
+++ app/Views/Perfil/Actualizar.php
@@ -9,40 +9,40 @@
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label >Primer nombre: </label>
-                    <input type="text" name="primer_nombre" class="form-control is-invalid" required>
+                    <input value="<?php $dato["primer_nombre"] ?>" type="text" name="primer_nombre" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label >Segundo nombre: </label>
-                    <input type="text" name="segundo_nombre" class="form-control is-invalid" required>
+                    <input value="<?php $dato["segundo_nombre"] ?>" type="text" name="segundo_nombre" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Primer apellido</label>
-                    <input type="text" name="primer_apellido" class="form-control is-invalid" required>
+                    <input value="<?php $dato["primer_apellido"] ?>" type="text" name="primer_apellido" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Segundo apellido:</label>
-                    <input type="text" name="segundo_apellido" class="form-control is-invalid" required>
+                    <input value="<?php $dato["segundo_apellido"] ?>" type="text" name="segundo_apellido" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Fecha de nacimiento:</label>
-                    <input type="date" name="fecha_nac" class="form-control is-invalid" required>
+                    <input value="<?php $dato["fecha_nac"] ?>" type="date" name="fecha_nac" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Cédula:</label>
-                    <input type="text" name="cedula" class="form-control is-invalid" required>
+                    <input value="<?php $dato["cedula"] ?>" type="text" name="cedula" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-left: 2%; padding-right: 2%;">
                     <label>Tipo de sangre:</label>
-                    <select name="tipo_sangre" class="custom-select is-invalid" required>
+                    <select value="<?php $dato["tipo_sangre"] ?>" name="tipo_sangre" class="custom-select is-invalid" required>
                         <option selected disabled value="">Seleccione...</option>
                         <option value="">A+</option>
                         <option value="">A-</option>
@@ -56,7 +56,7 @@
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Estado civíl:</label>
-                    <select name="estado_civil" class="custom-select is-invalid" id="validationServer04" required>
+                    <select value="<?php $dato["estado_civil"] ?>" name="estado_civil" class="custom-select is-invalid" id="validationServer04" required>
                         <option selected disabled value="">Seleccione...</option>
                         <option>Soltero(a)</option>
                         <option>Casado(a)</option>
@@ -68,7 +68,7 @@
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Género:</label>
-                    <select name="genero" class="custom-select is-invalid" required>
+                    <select value="<?php $dato["genero"] ?>" name="genero" class="custom-select is-invalid" required>
                         <option selected disabled value="">Seleccione...</option>
                         <option>Femenino</option>
                         <option>Masculino</option>
@@ -76,47 +76,47 @@
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Correo:</label>
-                    <input type="text" name="correo" class="form-control is-invalid" required>
+                    <input value="<?php $dato["correo"] ?>" type="email" name="correo" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Celular:</label>
-                    <input type="text" name="celular" class="form-control is-invalid" required>
+                    <input value="<?php $dato["celular"] ?>" type="text" name="celular" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Teléfono residencial:</label>
-                    <input type="text" name="telefono_casa" class="form-control is-invalid" required>
+                    <input value="<?php $dato["telefono_casa"] ?>" type="text" name="telefono_casa" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Provincia donde reside:</label>
-                    <input type="text" name="provincia_res" class="form-control is-invalid" required>
+                    <input value="<?php $dato["provincia_res"] ?>" type="text" name="provincia_res" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Distrito donde reside:</label>
-                    <input type="text" name="distrito_res" class="form-control is-invalid" required>
+                    <input value="<?php $dato["distrito_res"] ?>" type="text" name="distrito_res" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Corregimiento donde reside:</label>
-                    <input type="text" name="corregimiento_res" class="form-control is-invalid" required>
+                    <input value="<?php $dato["corregimiento_res"] ?>" type="text" name="corregimiento_res" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Dirección específica:</label>
-                    <input type="text" name="direccion_especif" class="form-control is-invalid" required>
+                    <input value="<?php $dato["direccion_especif"] ?>" type="text" name="direccion_especif" class="form-control is-invalid" required>
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Categoría:</label>
-                    <select class="custom-select is-invalid" name="categoria_doc" required>
+                    <select value="<?php $dato["categoria_doc"] ?>" class="custom-select is-invalid" name="categoria_doc" required>
                         <option selected disabled value="">Seleccione...</option>
                         <option value="1">Tiempo Completo</option>
                         <option value="2">Tiempo Parcial</option>
@@ -125,7 +125,7 @@
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                     <label>Sede:</label>
-                    <select class="custom-select is-invalid" name="ubicacion_sede" required>
+                    <select value="<?php $dato["ubicacion_sede"] ?>" class="custom-select is-invalid" name="ubicacion_sede" required>
                         <option selected disabled value="">Seleccione...</option>
                         <option>Sede central. Campus Dr. Victor Levi Sasso</option>
                         <option>Tocumen</option>
@@ -144,7 +144,7 @@
             <div class="form-row">
                 <div class="form-group col-md-6 mb-3" style="padding-left: 2%; padding-right: 2%;">
                     <label>Departamento:</label>
-                    <input type="text" name="depto" class="form-control is-invalid" required>
+                    <input value="<?php $dato["depto"] ?>" type="text" name="depto" class="form-control is-invalid" required>
                 </div>
                 <div class="form-group col-md-6 mb-3"  style="padding-left: 2%; padding-right: 2%;">
                 </div>
